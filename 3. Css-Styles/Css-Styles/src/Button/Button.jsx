99ce0fbb201ce012591d8762b86e6c146962ae77
noaru35 <vinicius.s.bastos55@gmail.com>
2024-09-09 22:01:29 -0300

    Updating

---
+++ 3. Css-Styles/Css-Styles/src/Button/Button.jsx
@@ -0,0 +1,18 @@
+
+function Button(){
+
+    const styles = {
+        backgroundColor: "rgb(155, 137, 114)",
+        color: "azure",
+        padding: "10px 20px",
+        borderRadius: "5px",
+        border: "none",
+        cursor: "pointer",
+    }
+
+    return( 
+        <button style={styles}>Click Me</button>
+    );
+}
+
+export default Button
