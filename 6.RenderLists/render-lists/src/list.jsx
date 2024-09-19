faf85e8b2051359cf6e9016db18f043965f0f61d
noaru35 <vinicius.s.bastos55@gmail.com>
2024-09-18 22:02:54 -0300

    updating

--- 6.RenderLists/render-lists/src/list.jsx
+++ 6.RenderLists/render-lists/src/list.jsx
@@ -1,16 +1,29 @@
+import PropTypes from  'prop-types';
 
-function List(){
 
-    const fruits = [{id:1, name: "apple", calories: 95},
-                     {id: 2, name: "orange", calories: 45},
-                     {id: 3, name: "banana", calories: 105},
-                     {id: 4, name: "coconut", calories: 159},
-                     {id: 5, name: "pineapple", calories: 37}];
+function List(props){
 
-    const listItems = fruits.map(fruit => <li key={fruit.name}>
-                                            {fruit.name}: &nbsp;
-                                            <b>{fruit.calories}</b></li>);
+    const category = props.category;
+    const itemList = props.items;
 
-    return(<ul>{listItems}</ul>);
+    const listItems = itemList.map(item => <li key={item.name}>
+                                                      {item.name}: &nbsp;
+                                                      <b>{item.calories}</b></li>);
+
+    return(<>
+            <h3 className="list-category">{category}</h3>
+            <ul className="list-items">{listItems}</ul>
+        </>);
+}
+
+List.propTypes ={
+    category: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({id: PropTypes.number,
+                                              name: PropTypes.string,
+                                            calories: PropTypes.number})),
+}
+List.defaultProps = {
+    category: "Category",
+    items: [],
 }
 export default List;
